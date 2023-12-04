a3ff95133c457febb3b1f9fadcb91c098d26b079
Nestibrave <36013379+Nestibrave@users.noreply.github.com>
2023-12-03 21:32:35 -0300

    Archivos añadidos mediante arrastre

---
+++ Scripts/Sonia.gd
@@ -0,0 +1,116 @@
+extends CharacterBody2D
+
+@export var speed : float
+var run : bool = false
+
+func _process(delta):
+	Motion_ctrl()
+	pass
+	
+func Motion_ctrl():
+	Direction_ctrl()
+	Animation_ctrl()
+	Speed_ctrl()
+	move_and_slide()
+	pass
+	
+func Direction_ctrl():
+	velocity.x = Globalscript.get_axis().x * speed
+	velocity.y = Globalscript.get_axis().y * -speed
+	pass
+	
+func Animation_ctrl():
+	Walk_Run_animation_ctrl()
+	pass
+	
+func Walk_Run_animation_ctrl():
+	if run:
+		Run_animation_ctrl()
+	else:
+		Walk_animation_ctrl()
+		pass
+	
+func Run_animation_ctrl():
+	if Input.is_action_pressed("ui_right"):
+		if Input.is_action_pressed("ui_up"):
+			$AnimationPlayer.play("run_up_right")
+		elif Input.is_action_pressed("ui_down"):
+			$AnimationPlayer.play("run_down_right")
+		else:
+			$AnimationPlayer.play("run_right")
+			pass
+	elif Input.is_action_pressed("ui_left"):
+		if Input.is_action_pressed("ui_up"):
+			$AnimationPlayer.play("run_up_left")
+		elif Input.is_action_pressed("ui_down"):
+			$AnimationPlayer.play("run_down_left")
+		else:
+			$AnimationPlayer.play("run_left")
+			pass
+	elif Input.is_action_pressed("ui_up"):
+		if Input.is_action_pressed("ui_left"):
+			$AnimationPlayer.play("run_up_left")
+		elif Input.is_action_pressed("ui_right"):
+			$AnimationPlayer.play("run_up_right")
+		else:
+			$AnimationPlayer.play("run_up")
+			pass
+	elif Input.is_action_pressed("ui_down"):
+		if Input.is_action_pressed("ui_left"):
+			$AnimationPlayer.play("run_down_left")
+		elif Input.is_action_pressed("ui_right"):
+			$AnimationPlayer.play("run_down_right")
+		else:
+			$AnimationPlayer.play("run_down")
+			pass
+	else:
+		$AnimationPlayer.play("idle")
+		pass
+	pass
+	
+func Walk_animation_ctrl():
+	if Input.is_action_pressed("ui_right"):
+		if Input.is_action_pressed("ui_up"):
+			$AnimationPlayer.play("walk_up_right")
+		elif Input.is_action_pressed("ui_down"):
+			$AnimationPlayer.play("walk_down_right")
+		else:
+			$AnimationPlayer.play("walk_right")
+			pass
+	elif Input.is_action_pressed("ui_left"):
+		if Input.is_action_pressed("ui_up"):
+			$AnimationPlayer.play("walk_up_left")
+		elif Input.is_action_pressed("ui_down"):
+			$AnimationPlayer.play("walk_down_left")
+		else:
+			$AnimationPlayer.play("walk_left")
+			pass
+	elif Input.is_action_pressed("ui_up"):
+		if Input.is_action_pressed("ui_left"):
+			$AnimationPlayer.play("walk_up_left")
+		elif Input.is_action_pressed("ui_right"):
+			$AnimationPlayer.play("walk_up_right")
+		else:
+			$AnimationPlayer.play("walk_up")
+			pass
+	elif Input.is_action_pressed("ui_down"):
+		if Input.is_action_pressed("ui_left"):
+			$AnimationPlayer.play("walk_down_left")
+		elif Input.is_action_pressed("ui_right"):
+			$AnimationPlayer.play("walk_down_right")
+		else:
+			$AnimationPlayer.play("walk_down")
+			pass
+	else:
+		$AnimationPlayer.play("idle")
+		pass
+	
+func Speed_ctrl():
+	if Input.is_action_just_pressed("Run"):
+		speed = 200
+		run = true
+	elif Input.is_action_just_released("Run"):
+		speed = 120
+		run = false
+		pass
+	pass
